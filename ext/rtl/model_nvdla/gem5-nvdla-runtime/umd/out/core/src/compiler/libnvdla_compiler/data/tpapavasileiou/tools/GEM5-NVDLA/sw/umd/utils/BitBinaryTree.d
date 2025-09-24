0bc392371c95635de3e8fe82cd79ad779249a438
tpapavas <tpapavas@csd.auth.gr>
2025-09-24 19:20:26 +0300

    ext: added nvdla/sw files to port to verilated dla
    Change-Id: Id974f83708cef3df265ce4524ffa0318d49a74b0

---
+++ ext/rtl/model_nvdla/gem5-nvdla-runtime/umd/out/core/src/compiler/libnvdla_compiler/data/tpapavasileiou/tools/GEM5-NVDLA/sw/umd/utils/BitBinaryTree.d
@@ -0,0 +1,32 @@
+/data/tpapavasileiou/tools/GEM5-NVDLA/sw/umd/out/core/src/compiler/libnvdla_compiler//data/tpapavasileiou/tools/GEM5-NVDLA/sw/umd/utils/BitBinaryTree.o: \
+ /data/tpapavasileiou/tools/GEM5-NVDLA/sw/umd/utils/BitBinaryTree.c \
+ /usr/include/stdc-predef.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/stdbool.h \
+ /data/tpapavasileiou/tools/GEM5-NVDLA/sw/umd/core/include/dlaerror.h \
+ /data/tpapavasileiou/tools/GEM5-NVDLA/sw/umd/core/include/dlatypes.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h /usr/include/stdlib.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/stddef.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn-common.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-float.h /usr/include/memory.h \
+ /usr/include/string.h \
+ /data/tpapavasileiou/tools/GEM5-NVDLA/sw/umd/core/include/nvdla_os_inf.h \
+ /usr/include/dirent.h /usr/include/x86_64-linux-gnu/bits/dirent.h \
+ /usr/include/x86_64-linux-gnu/bits/dirent_ext.h \
+ /data/tpapavasileiou/tools/GEM5-NVDLA/sw/umd/core/include/dlaerror.h \
+ /data/tpapavasileiou/tools/GEM5-NVDLA/sw/umd/core/include/dlatypes.h \
+ /data/tpapavasileiou/tools/GEM5-NVDLA/sw/umd/core/include/BitBinaryTree.h \
+ /data/tpapavasileiou/tools/GEM5-NVDLA/sw/umd/core/include/ErrorMacros.h
---
+++ ext/rtl/model_nvdla/gem5-nvdla-runtime/umd/out/core/src/compiler/libnvdla_compiler/home/tpapavasileiou/GEM5/gem5-dev/ext/rtl/model_nvdla/gem5-nvdla-runtime/umd/utils/BuddyAlloc.d
@@ -0,0 +1,32 @@
+/home/tpapavasileiou/GEM5/gem5-dev/ext/rtl/model_nvdla/gem5-nvdla-runtime/umd/out/core/src/compiler/libnvdla_compiler//home/tpapavasileiou/GEM5/gem5-dev/ext/rtl/model_nvdla/gem5-nvdla-runtime/umd/utils/BuddyAlloc.o: \
+ /home/tpapavasileiou/GEM5/gem5-dev/ext/rtl/model_nvdla/gem5-nvdla-runtime/umd/utils/BuddyAlloc.c \
+ /usr/include/stdc-predef.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/stdbool.h /usr/include/stdlib.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/stddef.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn-common.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-float.h \
+ /home/tpapavasileiou/GEM5/gem5-dev/ext/rtl/model_nvdla/gem5-nvdla-runtime/umd/core/include/dlaerror.h \
+ /home/tpapavasileiou/GEM5/gem5-dev/ext/rtl/model_nvdla/gem5-nvdla-runtime/umd/core/include/dlatypes.h \
+ /home/tpapavasileiou/GEM5/gem5-dev/ext/rtl/model_nvdla/gem5-nvdla-runtime/umd/core/include/nvdla_os_inf.h \
+ /usr/include/dirent.h /usr/include/x86_64-linux-gnu/bits/dirent.h \
+ /usr/include/x86_64-linux-gnu/bits/dirent_ext.h \
+ /home/tpapavasileiou/GEM5/gem5-dev/ext/rtl/model_nvdla/gem5-nvdla-runtime/umd/core/include/dlaerror.h \
+ /home/tpapavasileiou/GEM5/gem5-dev/ext/rtl/model_nvdla/gem5-nvdla-runtime/umd/core/include/dlatypes.h \
+ /home/tpapavasileiou/GEM5/gem5-dev/ext/rtl/model_nvdla/gem5-nvdla-runtime/umd/core/include/BuddyAlloc.h \
+ /home/tpapavasileiou/GEM5/gem5-dev/ext/rtl/model_nvdla/gem5-nvdla-runtime/umd/core/include/BitBinaryTree.h \
+ /home/tpapavasileiou/GEM5/gem5-dev/ext/rtl/model_nvdla/gem5-nvdla-runtime/umd/core/include/ErrorMacros.h
