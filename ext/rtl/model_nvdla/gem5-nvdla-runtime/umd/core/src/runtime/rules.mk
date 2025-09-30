73909c0f6962b90f68a4c8785da99a660a8e88e9
tpapavas <tpapavas@csd.auth.gr>
2025-09-30 19:16:31 +0300

    ext: adding kmd files to umd for gem5_nvdla_runtime
    This is another transitional commit in an
    attempt to turn kmd dla runtime to umd code.
    
    Change-Id: I90e1a15e37da90d309e02edc4311d7a8bfdbb616

--- ext/rtl/model_nvdla/gem5-nvdla-runtime/umd/core/src/runtime/rules.mk
+++ ext/rtl/model_nvdla/gem5-nvdla-runtime/umd/core/src/runtime/rules.mk
@@ -42,6 +42,21 @@ NVDLA_RUNTIME_SRC_FILES := \
     $(ROOT)/core/src/common/Loadable.cpp \
     $(ROOT)/port/linux/nvdla.c \
     $(ROOT)/port/linux/nvdla_os.c \
+    $(ROOT)/port/linux/gem5_nvdla_gem.c \
+    $(ROOT)/port/linux/gem5_nvdla_core_callbacks.c \
+    $(ROOT)/firmware/scheduler.c \
+    $(ROOT)/firmware/engine.c \
+    $(ROOT)/firmware/bdma.c \
+    $(ROOT)/firmware/conv.c \
+    $(ROOT)/firmware/sdp.c \
+    $(ROOT)/firmware/cdp.c \
+    $(ROOT)/firmware/pdp.c \
+    $(ROOT)/firmware/rubik.c \
+    $(ROOT)/firmware/cache.c \
+    $(ROOT)/firmware/common.c \
+    $(ROOT)/firmware/engine_data.c \
+    $(ROOT)/firmware/engine_isr.c \
+    $(ROOT)/firmware/engine_debug.c \
     Emulator.cpp \
     Runtime.cpp
 
@@ -51,6 +66,7 @@ INCLUDES += \
     -I$(ROOT)/core/src/common/include \
     -I$(ROOT)/port/linux/include \
     -I$(ROOT)/external/include \
+    -I$(ROOT)/firmware/include \
     -I$(LOCAL_DIR)/include \
     -I$(LOCAL_DIR)
 
