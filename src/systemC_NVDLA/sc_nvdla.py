27db87e05026f7644304bdc0a49d33ed6e5d4576
tpapavas <tpapavas@csd.auth.gr>
2026-03-24 20:41:19 +0200

    ext,dev: SC modules fixed for gem5-SC nvdla runtime support
    Change-Id: I57280a55534b05cfc641a721d36021f8a387994a

--- src/systemC_NVDLA/sc_nvdla.py
+++ src/systemC_NVDLA/sc_nvdla.py
@@ -9,12 +9,10 @@ from m5.params import *
 
 #     tlm = TlmTargetSocket(32, "TLM target socket")
 #     _csb_socket = TlmTargetSocket(32, "CSB  target socket")
-#     _dbb_socket = TlmInitiatorSocket(
-#       32, "DBB Initiator Socket (off-chip memory)"
-#     )
-#     _sram_socket= TlmInitiatorSocket(
-#       32, "SRAM Initiator Socket (on-chip memory)"
-#     )
+#     _dbb_socket = TlmInitiatorSocket(32,
+#           "DBB Initiator Socket (off-chip memory)")
+#     _sram_socket= TlmInitiatorSocket(32,
+#           "SRAM Initiator Socket (on-chip memory)")
 #     system = Param.System(Parent.any, "system")
 
 
@@ -39,6 +37,7 @@ class TLM_Target(SystemC_ScModule):
     cxx_class = "Target"
     cxx_header = "systemC_NVDLA/sc_tlm_target.hh"
     tlm = TlmTargetSocket(32, "TLM target socket")
+    tlm_i = TlmInitiatorSocket(32, "TLM initiator socket")
     system = Param.System(Parent.any, "system")
 
 
@@ -54,5 +53,33 @@ class TLM_ScNvDlaSE(SystemC_ScModule):
     type = "TLM_ScNvDlaSE"
     cxx_class = "ScNvDlaSE"
     cxx_header = "systemC_NVDLA/sc_nvdla_se.hh"
-    tlm = TlmTargetSocket(32, "TLM NVDLA target socket")
+
+    csb_target = TlmTargetSocket(32, "TLM NVDLA target socket")
+
+    dbb_init = TlmInitiatorSocket(32, "Off chip memory ")
+    sram_init = TlmInitiatorSocket(32, " Dedicated On-chip Cache ")
+
+    dbb_target = TlmTargetSocket(32, "Off chip memory ")
+    sram_target = TlmTargetSocket(32, " Dedicated On-chip Cache ")
+
     system = Param.System(Parent.any, "system")
+
+
+####################################################################
+# class ScNvDlaSE(SimObject):
+#     type = "ScNvDlaSE"
+#     cxx_class = "gem5::ScNvDlaSE"
+#     cxx_header = "systemC_NVDLA/sc_nvdla_se.hh"
+
+#     system = Param.System(Parent.any, "system reference")
+
+#     # module = Param.ScNVDLAModule("SystemC module")
+#     transactor  = Param.Gem5ToTlmBridge32(Parent.any, "CSB bridge")
+#     tlm_target = Param.TLM_Target("TLM target (CSB)")
+#     tlm_initiator = Param.TLM_Initiator("TLM initiator (for CSB)")
+
+#     accel_port = ResponsePort("CPU -> NVDLA trigger port")
+
+#     # csb  = Param.Gem5ToTlmBridge32(Parent.any, "CSB bridge")
+#     # sram = Param.TlmToGem5Bridge32(Parent.any, "SRAM bridge")
+#     # dbb  = Param.TlmToGem5Bridge32(Parent.any, "DBB bridge")
--- src/systemC_NVDLA/sc_tlm_target.cc
+++ src/systemC_NVDLA/sc_tlm_target.cc
@@ -43,7 +43,9 @@
 #include "params/TLM_Target.hh"
 #include "sc_tlm_target.hh"
 
-#include "systemc/ext/systemc"
+//#include "systemc/ext/systemc"
+#include "systemc.h"
+
 #include "systemc/ext/tlm"
 
 using namespace std;
@@ -53,69 +55,27 @@ using namespace gem5;
 void Target::b_transport(tlm::tlm_generic_payload& trans,
                             sc_time& delay)
 {
-    executeTransaction(trans);
+    std::cout << "[NVDLA -> DRAM] addr=0x"
+              << std::hex << trans.get_address()
+              << " cmd=" << (trans.is_read() ? "READ" : "WRITE")
+              << std::endl;
+
+    if (!iSocket.get_interface()) {
+        std::cout << "[Error] no interface isocket";
+    }
+    iSocket->b_transport(trans, delay);
+    std::cout << "after tSocket->b_transport(trans, delay);\n ";
+
+
+    //uint32_t data = *reinterpret_cast<uint32_t*>(trans.get_data_ptr());
+    //std::cout << "[NVDLA -> DRAM] data are : " << std::hex << data << "\n";
+    //std::cout << " isocket size are " << iSocket.size() << "\n";
+    //iSocket->b_transport(trans, delay);
+
 }
 
-void Target::executeTransaction(tlm::tlm_generic_payload& trans)
-{
-    tlm::tlm_command cmd = trans.get_command();
-    sc_dt::uint64    adr = trans.get_address();
-    unsigned char*   ptr = trans.get_data_ptr();
-    unsigned int     len = trans.get_data_length();
-    unsigned char*   byt = trans.get_byte_enable_ptr();
 
-    if (trans.get_address() >= 16*1024*1024) {
-        cout << "\033[1;31m("
-             << "Address Error"
-             << "\033[0m" << endl;
-        trans.set_response_status( tlm::TLM_ADDRESS_ERROR_RESPONSE );
-        return;
-    }
-    if (byt != 0) {
-        cout << "\033[1;31m("
-             << "Byte Enable Error"
-             << "\033[0m" << endl;
-        trans.set_response_status( tlm::TLM_BYTE_ENABLE_ERROR_RESPONSE );
-        return;
-    }
-    if (len < 1 || len > 64) {
-        cout << "\033[1;31m("
-             << "Burst Error"
-             << "\033[0m" << endl;
-        trans.set_response_status( tlm::TLM_BURST_ERROR_RESPONSE );
-        return;
-    }
 
-    if (cmd == tlm::TLM_READ_COMMAND)
-    {
-        memcpy(mem+trans.get_address(), // destination
-                trans.get_data_ptr(),      // source
-                trans.get_data_length());  // size
-    }
-    else if (cmd == tlm::TLM_WRITE_COMMAND)
-    {
-        memcpy(trans.get_data_ptr(),      // destination
-                mem + trans.get_address(), // source
-                trans.get_data_length());  // size
-    }
-
-    cout << "\033[1;32m("
-            << name()
-            << ")@"  << setfill(' ') << setw(12) << sc_time_stamp()
-            << ": " << setw(12) << (cmd ? "Exec. Write " : "Exec. Read ")
-            << "Addr = " << setfill('0') << setw(8) << hex << adr
-            << " Data = " << "0x" << setfill('0') << setw(8) << hex
-            << *reinterpret_cast<int*>(ptr)
-            << "\033[0m" << endl;
-
-    trans.set_response_status( tlm::TLM_OK_RESPONSE );
-}
-
-// This "create" method bridges the python configuration and the systemc
-// objects. It instantiates the Printer object and sets it up using the
-// parameter values from the config, just like it would for a SimObject. The
-// systemc object could accept those parameters however it likes, for instance
-// through its constructor or by assigning them to a member variable.
 Target *
 gem5::TLM_TargetParams::create() const
 {
@@ -126,5 +86,11 @@ gem5::TLM_TargetParams::create() const
 gem5::Port
 &Target::gem5_getPort(const std::string &if_name, int idx)
 {
+    if (if_name == "tlm_i"){
+        std::cout << " [gem5::Port &Target::gem5_getPort] i_wrapper\n";
+        return i_wrapper;
+    }
+
     return wrapper;
+
 }
